975bdb3c32835a9453c12b4434535bad3683e2af
Efim Tumanov <48530270+ijustseeblood@users.noreply.github.com>
2020-02-07 09:50:42 +0300

    Add files via upload

---
+++ JsonTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Tests
+{
+    class JsonTest
+    {
+        public void CorrectJson(string source, string dest)
+        {
+            string jsonString = File.ReadAllText(source);
+            char[] fakeJsonString = jsonString.ToCharArray();
+            foreach (char item in fakeJsonString)
+            {
+                if (item == '(') { fakeJsonString[item] = '['; }
+                else if (item == ')') { fakeJsonString[item] = ']'; }
+            }
+            jsonString = new string(fakeJsonString);
+            File.WriteAllText(dest, jsonString);
+        }
+    }
+}
